7546bfd36fa8b30b039eb0077dd7629cc8f8c137
D3K31Z3R <benjamin.belderbos@hotmail.com>
2020-03-30 13:12:39 +0200

    a

--- NotesApp/src/app/app.module.ts
+++ NotesApp/src/app/app.module.ts
@@ -2,18 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from "@angular/common/http";
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserComponent } from './user/user.component';
+import { NoteComponent } from './note/note.component';
+import { CreateNoteComponent } from './create-note/create-note.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatTableModule} from "@angular/material/table";
 
 @NgModule({
 
   declarations: [
     AppComponent,
+    UserComponent,
+    NoteComponent,
+    CreateNoteComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, g, MatFormFieldModule, ReactiveFormsModule, FormsModule, MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
